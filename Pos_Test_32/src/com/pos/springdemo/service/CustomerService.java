b5487da6f13871d5eb86d4ca958cc8d6f67abf18
Emir Luna <emir.luna@outlook.com>
2020-07-04 14:08:32 -0500

    First service layer

---
+++ Pos_Test_32/src/com/pos/springdemo/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.pos.springdemo.service;
+
+import java.util.List;
+
+import com.pos.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+}
